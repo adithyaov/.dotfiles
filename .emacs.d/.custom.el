1c8ca8d840aa7d041eddd5b830e83405d0854fa3
adithyaov <adi.obilisetty@gmail.com>
2020-08-16 12:39:16 +0530

    Add ~/.emacs.d/.custom.el

---
+++ .emacs.d/.custom.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" default)))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme spacemacs-theme counsel-projectile flymake-hlint hlint-refactor ormolu column-enforce-mode column-marker ahk-mode csharp-mode eshell-git-prompt highlight-indent-guides hindent structured-haskell-mode impatient-mode markdown-preview-eww flymd use-package symon sublime-themes rebecca-theme projectile plan9-theme nimbus-theme multiple-cursors magit hydra haskell-mode gruber-darker-theme expand-region dracula-theme doom-themes crux counsel company avy ample-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "#FFFFEF" :foreground "#303030"))))
+ '(eshell-git-prompt-robyrussell-branch-face ((t (:background "red" :foreground "white"))))
+ '(eshell-git-prompt-robyrussell-git-face ((t (:background "blue" :foreground "white"))))
+ '(ivy-current-match ((t (:background "#000000" :foreground "#F0DFAF" :underline t :weight bold))))
+ '(ivy-highlight-face ((t (:background nil :foreground nil))))
+ '(ivy-minibuffer-match-face-2 ((t (:background "#5F7F5F" :foreground "#F0DFAF"))))
+ '(ivy-minibuffer-match-face-3 ((t (:background "#7F9F7F" :foreground "#F0DFAF"))))
+ '(ivy-minibuffer-match-face-4 ((t (:background "#8FB28F" :foreground "#FFFFFF"))))
+ '(magit-diff-added ((t (:background "#AFF3AF" :foreground "#519F50" :weight light))))
+ '(magit-diff-added-highlight ((t (:background "#A4E0A4" :foreground "#519F50" :weight bold))))
+ '(magit-diff-removed ((t (:background "#D87B7B" :foreground "#EF2F27" :weight light))))
+ '(magit-diff-removed-highlight ((t (:background "#DE9191" :foreground "#EF2F27" :weight bold)))))
