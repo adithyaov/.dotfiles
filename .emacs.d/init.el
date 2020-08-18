cf38a36921daf5e270b8869e87bc2b69b7941580
adithyaov <adi.obilisetty@gmail.com>
2020-08-18 13:41:26 +0530

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,25 +3,28 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (global-display-line-numbers-mode)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
 (blink-cursor-mode 1)
 (global-hl-line-mode 1)
 
-;; Package configs
-(require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
-                         ("gnu"   . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
+;; Ignore ding
+(setq ring-bell-function 'ignore)
 
-;; Remapping bindings to the max
-;(global-set-key (kbd "C-c SPC") 'set-mark-command)
+;; Onsave hook, remove spaces
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;(global-set-key (kbd "C-l") 'delete-backward-char)
-;(global-set-key (kbd "M-l") 'backward-kill-word)
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 ;; scroll one line at a time (less "jumpy" than defaults)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
@@ -29,259 +32,207 @@
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 (setq scroll-step 1) ;; keyboard scroll one line at a time
 
-;; Bootstrap `use-package`
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-
+;; Key bindings
 (global-set-key (kbd "M-o") 'other-window)
-(global-set-key (kbd "C-f") 'forward-char)
-;; (define-key isearch-mode-map "\C-f" 'isearch-repeat-forward)
 (define-key isearch-mode-map "\C-n" 'isearch-repeat-forward)
-(global-set-key (kbd "C-b") 'backward-char)
-;; (define-key isearch-mode-map "\C-b" 'isearch-repeat-backward)
 (define-key isearch-mode-map "\C-p" 'isearch-repeat-backward)
-(global-set-key (kbd "M-f") 'forward-word)
-(global-set-key (kbd "M-b") 'backward-word)
-
 (global-set-key (kbd "M-n") 'forward-paragraph)
 (global-set-key (kbd "M-p") 'backward-paragraph)
-
-;; (global-set-key (kbd "C-j") 'backward-delete-char)
-;; (global-set-key (kbd "C-d") 'backward-delete-char)
-;; (global-set-key (kbd "M-d") 'ivy-backward-kill-word)
-
 (global-set-key (kbd "C-s") 'isearch-forward)
 (global-set-key (kbd "C-r") 'isearch-backward)
+
+; Use Alt+Arrow to jump to different windows
+(windmove-default-keybindings 'meta)
+
+; Default white space to match anything
 (setq search-whitespace-regexp ".*?")
 (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
 
+; Hilight matching parenthesis
 (progn
   (setq show-paren-style 'parenthesis)
   (show-paren-mode 1))
 
+;; Goto program dir when you type "prog"
 (defun goto-prog-dir ()
   "Change directory to the Desktop/LinuxWorkStation/Prog"
   (interactive)
   (cd "/mnt/c/Users/mota/Desktop/LinuxWorkStation/Prog"))
-
-(defalias 'open 'find-file)
-(defalias 'openo 'find-file-other-window)
 (defalias 'prog 'goto-prog-dir)
 
-(defconst HOME "/home/creed")
+;; Set the HOME constant
+(defconst HOME (getenv "HOME"))
 
-(define-key key-translation-map (kbd "C-c x") (kbd "C-x"))
-
-;; (global-set-key (kbd "C-f") 'forward-word)
-;; (global-set-key (kbd "M-f") 'forward-char)
-;; (global-set-key (kbd "C-b") 'backward-word)
-;; (global-set-key (kbd "M-b") 'backward-char)
-
+;; Set column length to 80
 (setq-default fill-column 80)
 
-(use-package ahk-mode
-  :ensure t)
+;; Imports
+(straight-use-package 'ahk-mode)
+(straight-use-package 'column-enforce-mode)
+(straight-use-package 'eshell-git-prompt)
+(straight-use-package 'csharp-mode)
+(straight-use-package 'highlight-indent-guides)
+(straight-use-package 'solarized-theme)
+(straight-use-package 'impatient-mode)
+(straight-use-package 'expand-region)
+(straight-use-package 'ivy)
+(straight-use-package 'swiper)
+(straight-use-package 'counsel)
+(straight-use-package 'magit)
+(straight-use-package 'haskell-mode)
+(straight-use-package 'hindent)
+(straight-use-package 'projectile)
+(straight-use-package 'counsel-projectile)
+(straight-use-package 'org)
+(straight-use-package 'multiple-cursors)
+(straight-use-package 'avy)
+(straight-use-package 'hydra)
+(straight-use-package 'rust-mode)
+(straight-use-package 'highlight-function-calls)
 
-(use-package column-enforce-mode
-  :ensure t
-  :config (global-column-enforce-mode t))
+;; Hilight text that extends beyond a certain column
+(progn
+  (require 'column-enforce-mode)
+  (global-column-enforce-mode t))
 
-(use-package eshell-git-prompt
-  :ensure t
-  :config (eshell-git-prompt-use-theme 'robbyrussell))
+;; Git prompt in eshell
+(progn
+  (require 'eshell-git-prompt)
+  (eshell-git-prompt-use-theme 'robbyrussell))
 
-(use-package csharp-mode
-  :ensure t)
+;; Show indentation block
+(progn
+  (require 'highlight-indent-guides)
+  (setq highlight-indent-guides-method 'character)
+  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
 
-(use-package highlight-indent-guides
-  :ensure t
-  :config (setq highlight-indent-guides-method 'fill))
+;; Load theme
+(progn
+  (require 'solarized-theme)
+  (load-theme 'solarized-dark t))
 
-;; Themes START --------------------------------------------
-
-;(use-package srcery-theme :ensure t)
-;(use-package gruber-darker-theme :ensure t)
-
-;(use-package zenburn-theme :ensure t)
-;;(custom-set-faces
-;; '(cursor ((t (:background "#FFFFEF" :foreground "#303030"))))
-;; '(eshell-git-prompt-robyrussell-branch-face ((t (:background nil :foreground "#DE9191"))))
-;; '(eshell-git-prompt-robyrussell-git-face ((t (:background nil :foreground "#9197DE"))))
-;; '(ivy-current-match ((t (:background "#000000" :foreground "#F0DFAF" :underline t :weight bold))))
-;; '(ivy-highlight-face ((t (:background nil :foreground nil))))
-;; '(ivy-minibuffer-match-face-2 ((t (:background "#5F7F5F" :foreground "#F0DFAF"))))
-;; '(ivy-minibuffer-match-face-3 ((t (:background "#7F9F7F" :foreground "#F0DFAF"))))
-;; '(ivy-minibuffer-match-face-4 ((t (:background "#8FB28F" :foreground "#FFFFFF"))))
-;; '(magit-diff-added ((t (:background "#BCFFAD" :foreground "#519F50" :weight light))))
-;; '(magit-diff-added-highlight ((t (:background "#BCFFAD" :foreground "#519F50" :weight bold))))
-;; '(magit-diff-removed ((t (:background "#FFADAD" :foreground "#EF2F27" :weight light))))
-;; '(magit-diff-removed-highlight ((t (:background "#FFADAD" :foreground "#EF2F27" :weight bold)))))
-
-;(use-package sublime-themes :ensure t)
-
-;(use-package leuven-theme
-;  :config
-;  (load-theme 'leuven-dark t))
-
-(use-package flatui-theme
-  :ensure t
-  :config
-  (load-theme 'flatui))
-
-;; Themes END ----------------------------------------------
-
-(use-package impatient-mode
-  :ensure t
-  :config
+;; Look at markdown in a clean format
+(progn
+  (require 'impatient-mode)
   (defun markdown-html (buffer)
-  (princ (with-current-buffer buffer
-    (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
-  (current-buffer))))
+    (princ (with-current-buffer buffer
+	     (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
+	   (current-buffer))))
 
-(setq ring-bell-function 'ignore)
 
-(use-package expand-region
-  :ensure t
-  :config
+
+(progn
+  (require 'expand-region)
   (pending-delete-mode t)
   (global-set-key (kbd "C-=") 'er/expand-region)
   (global-set-key (kbd "<C-return>") 'er/expand-region)
   (global-set-key (kbd "C--") 'er/contract-region))
 
-(use-package ivy
-  :ensure t
-  :config
+(progn
+  (require 'ivy)
+  (require 'counsel)
+  (require 'swiper)
   (ivy-mode 1)
   (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) "))
-
-(use-package swiper
-  :ensure t
-  :config)
-
-(use-package counsel
-  :ensure t
-  :config
+  (setq ivy-count-format "(%d/%d) ")
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "C-c g") 'counsel-git))
 
-(use-package magit
-  :ensure t
-  :config
+(progn
+  (require 'magit)
   (global-set-key (kbd "C-x g") 'magit-status))
 
-(use-package haskell-mode
-  :ensure t
-  :config
-  (setq haskell-tags-on-save nil
-	tags-revert-without-query t
-	haskell-indentation-mode nil
-	haskell-compile-cabal-build-command "cabal v2-build"))
+(progn
+  (require 'haskell-mode)
+  (setq haskell-tags-on-save nil)
+  (setq tags-revert-without-query t)
+  (setq haskell-indentation-mode nil)
+  (setq haskell-compile-cabal-build-command "cabal v2-build"))
 
-(use-package hindent
-  :ensure t
-  :config (add-hook 'haskell-mode-hook #'hindent-mode))
+(progn
+  (require 'hindent)
+  (add-hook 'haskell-mode-hook #'hindent-mode))
 
-; (use-package flymake-hlint
-;   :ensure t
-;   :config (add-hook 'haskell-mode-hook 'flymake-hlint-load))
-
-(use-package projectile
-  :ensure t
-  :config
+(progn
+  (require 'projectile)
+  (require 'ivy)
+  (require 'counsel)
+  (require 'swiper)
+  (require 'counsel-projectile)
   (projectile-mode +1)
+  (counsel-projectile-mode 1)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (setq projectile-completion-system 'ivy)
-  (setq projectile-project-search-path '("/mnt/c/Users/mota/Desktop/LinuxWorkStation/Prog/")))
+  (setq projectile-project-search-path
+	'("/mnt/c/Users/mota/Desktop/LinuxWorkStation/Prog/")))
 
-(use-package counsel-projectile
-  :ensure t
-  :config
-  (counsel-projectile-mode 1))
-
-(use-package org
-  :ensure t)
-
-(use-package multiple-cursors
-  :ensure t
-  :config
+(progn
+  (require 'multiple-cursors)
   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
 
-(use-package avy
-  :ensure t
-  :config
+(progn
+  (require 'avy)
   (setq avy-keys '(?a ?s ?d ?f ?q ?w ?e ?r ?n ?m ?g ?h ?j ?k ?l))
   (setq avy-background t)
   (setq avy-orders-alist '((avy-goto-word-0 . avy-order-closest)))
   (setq avy-all-windows nil)
   (global-set-key (kbd "M-RET") 'avy-goto-word-0))
 
-(windmove-default-keybindings 'meta)
 
 (defun open-my-init-file ()
   "Open the init file."
   (interactive)
   (find-file user-init-file))
 
-(use-package crux
-  :ensure t
-  :bind (("C-a" . crux-move-beginning-of-line)))
-
+;; Use C-a to move to beginning of line and first indentation
 (defun smarter-move-point ()
   "Move point to the first non-whitespace character on this line.
 If point is already there, move to the beginning of the line.
-If point is already there, move to the end of line.
-Effectively toggle between the first non-whitespace character,
-the beginning of the line and the end of the line."
+Effectively toggle between the first non-whitespace character and
+the beginning of the line"
   (interactive)
 
   (let ((orig-point (point)))
     (if (= orig-point (point-at-bol))
-      (move-end-of-line 1)
-      (progn (back-to-indentation)
-	     (when (= orig-point (point))
+      (back-to-indentation)
+      (progn (when (= orig-point (point))
 	       (move-beginning-of-line 1))))))
 
-(global-set-key (kbd "C-o") 'smarter-move-point)
+; Bind C-a to smarter-move-point
+(global-set-key (kbd "C-a") 'smarter-move-point)
 
+;; Open buffer list/switch
 (defun buffer-list-switch ()
   "Switch to buffer list and activate the window"
   (interactive)
   (list-buffers)
-  (select-window (get-buffer-window "*Buffer List*" 0))
-)
+  (select-window (get-buffer-window "*Buffer List*" 0)))
 
+; Bind C-x C-b to buffer-list-switch
 (global-set-key (kbd "C-x C-b") 'buffer-list-switch)
 
-(use-package hydra
-  :ensure t
-  :config
+(progn
+  (require 'hydra)
   (global-set-key
-    (kbd "C-c m")
-    (defhydra hydra-movement (:body-pre (set-mark-command nil))
-      "Hydra navigation"
-      ("f"   forward-char)
-      ("F"   forward-word)
-      ("b"   backward-char)
-      ("B"   backward-word)
-      ("p"   previous-line)
-      ("n"   next-line)
-      ("P"   backward-paragraph)
-      ("N"   forward-paragraph)
-      ("a"   crux-move-beginning-of-line)
-      ("e"   move-end-of-line)
-      ("q"   nil)
-      ("v"   scroll-up-command)
-      ("V"   scroll-down-command)
-      ("m"   avy-goto-word-0 :color blue)
-      )))
+   (kbd "C-c m")
+   (defhydra hydra-movement (:body-pre (set-mark-command nil))
+     "Hydra navigation"
+     ("f"   forward-char)
+     ("F"   forward-word)
+     ("b"   backward-char)
+     ("B"   backward-word)
+     ("p"   previous-line)
+     ("n"   next-line)
+     ("P"   backward-paragraph)
+     ("N"   forward-paragraph)
+     ("a"   crux-move-beginning-of-line)
+     ("e"   move-end-of-line)
+     ("q"   nil)
+     ("v"   scroll-up-command)
+     ("V"   scroll-down-command)
+     ("m"   avy-goto-word-0 :color blue))))
 
-(setq-default custom-file (expand-file-name ".custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
 
 ;; ===================================================================
 ;; Haskell auto show core script
@@ -324,3 +275,27 @@ the beginning of the line and the end of the line."
 	(unless (= (length f) 0)
 	  (dotfile-sync f)))
       (shell-command (concat git " push")))))
+
+;; ===================================================================
+;; cabal check open-repl/close
+(defun cabalcc (main &rest args)
+  (interactive "srepl: ")
+  (let* ((cmd (lambda (x) (concat "(echo :q | cabal repl " x  ")" )))
+	 (and-cmd (lambda (x) (concat " && " (funcall cmd x))))
+	 (result (funcall cmd main))
+	 (full-cmd
+	  (dolist (element args result)
+	    (setq result (concat result (funcall and-cmd element))))))
+    (projectile-with-default-dir (projectile-project-root)
+      (compile full-cmd))))
+
+;; Highlight emacs function calls
+(progn
+  (require 'highlight-function-calls)
+  (add-hook 'emacs-lisp-mode-hook 'highlight-function-calls-mode))
+
+;; Load custom file
+(progn
+  (setq-default custom-file (expand-file-name "custom.el" user-emacs-directory))
+  (when (file-exists-p custom-file)
+    (load custom-file)))
