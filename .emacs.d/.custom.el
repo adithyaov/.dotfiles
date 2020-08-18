9747240f2e73d22c13d832b01bd15ec3433a49dc
adithyaov <adi.obilisetty@gmail.com>
2020-08-18 13:41:26 +0530

    Update .custom.el

--- .emacs.d/.custom.el
+++ .emacs.d/.custom.el
@@ -8,10 +8,10 @@
     ("ae88c445c558b7632fc2d72b7d4b8dfb9427ac06aa82faab8d760fff8b8f243c" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" default)))
  '(package-selected-packages
    (quote
-    (flatui-theme leuven-theme emacs-leuven-theme zenburn-theme spacemacs-theme counsel-projectile flymake-hlint hlint-refactor ormolu column-enforce-mode column-marker ahk-mode csharp-mode eshell-git-prompt highlight-indent-guides hindent structured-haskell-mode impatient-mode markdown-preview-eww flymd use-package symon sublime-themes rebecca-theme projectile plan9-theme nimbus-theme multiple-cursors magit hydra haskell-mode gruber-darker-theme expand-region dracula-theme doom-themes crux counsel company avy ample-theme))))
+    (solarized-theme rust-mode flatui-theme leuven-theme emacs-leuven-theme zenburn-theme spacemacs-theme counsel-projectile flymake-hlint hlint-refactor ormolu column-enforce-mode column-marker ahk-mode csharp-mode eshell-git-prompt highlight-indent-guides hindent structured-haskell-mode impatient-mode markdown-preview-eww flymd use-package symon sublime-themes rebecca-theme projectile plan9-theme nimbus-theme multiple-cursors magit hydra haskell-mode gruber-darker-theme expand-region dracula-theme doom-themes crux counsel company avy ample-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-section-highlight ((t (:background "#dfe4ea")))))
+ '(highlight-function-calls-face ((t (:weight bold)))))
