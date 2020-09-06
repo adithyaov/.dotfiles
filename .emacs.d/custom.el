28df647e58c90b6e67635493586a2c2bed306e55
adithyaov <adi.obilisetty@gmail.com>
2020-09-07 03:06:46 +0530

    Update custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,6 +6,8 @@
  '(custom-safe-themes
    (quote
     ("ae88c445c558b7632fc2d72b7d4b8dfb9427ac06aa82faab8d760fff8b8f243c" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" default)))
+ '(haskell-process-type (quote cabal-repl))
+ '(major-mode (quote org-mode))
  '(package-selected-packages
    (quote
     (solarized-theme rust-mode flatui-theme leuven-theme emacs-leuven-theme zenburn-theme spacemacs-theme counsel-projectile flymake-hlint hlint-refactor ormolu column-enforce-mode column-marker ahk-mode csharp-mode eshell-git-prompt highlight-indent-guides hindent structured-haskell-mode impatient-mode markdown-preview-eww flymd use-package symon sublime-themes rebecca-theme projectile plan9-theme nimbus-theme multiple-cursors magit hydra haskell-mode gruber-darker-theme expand-region dracula-theme doom-themes crux counsel company avy ample-theme))))
