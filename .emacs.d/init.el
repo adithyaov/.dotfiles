775a6d81ccc398d413f0efe57deaac7eabb35bf5
adithyaov <adi.obilisetty@gmail.com>
2020-09-07 03:06:47 +0530

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -73,6 +73,9 @@
 ;; Set the HOME constant
 (defconst HOME (getenv "HOME"))
 
+;; Auth keys
+(setq auth-sources '("~/.authinfo"))
+
 ;; Set column length to 80
 (setq-default fill-column 80)
 
@@ -103,8 +106,12 @@
 (straight-use-package
  '(ox-reveal :type git :host github :repo "yjwen/Org-Reveal"))
 (straight-use-package 's)
+(straight-use-package 'dash)
 (straight-use-package 'company)
 (straight-use-package 'company-fuzzy)
+(straight-use-package 'forge)
+
+(require 'forge)
 
 (progn
   (require 'company)
@@ -114,8 +121,9 @@
   (setq company-eclim-auto-save nil)
   (setq company-dabbrev-downcase nil)
   (add-hook 'after-init-hook 'global-company-mode)
-  (global-set-key (kbd "M-/") 'company-dabbrev)
+  (global-set-key (kbd "M-/") 'company-complete)
   (global-company-fuzzy-mode 1)
+  (setq company-idle-delay nil)
   (setq company-fuzzy-prefix-ontop t))
 
 (require 'ox-reveal)
@@ -143,7 +151,7 @@
 ;; Load theme
 (progn
   (require 'solarized-theme)
-  (load-theme 'solarized-dark t))
+  (load-theme 'solarized-light t))
 
 ;; Look at markdown in a clean format
 (progn
@@ -252,9 +260,10 @@
 
 (progn
   (require 'avy)
-  (setq avy-keys '(?a ?s ?d ?f ?q ?w ?e ?r ?n ?m ?g ?h ?j ?k ?l))
+  (setq avy-keys '(?a ?s ?d ?f ?q ?w ?e ?r ?b ?n ?m ?j ?k ?l ?o ?p))
   (setq avy-background t)
-  (setq avy-orders-alist '((avy-goto-word-0 . avy-order-closest)))
+  (setq avy-orders-alist '((avy-goto-word-0 . avy-order-closest)
+			   (avy-goto-word-1 . avy-order-closest)))
   (setq avy-all-windows nil)
   (global-set-key (kbd "M-RET") 'avy-goto-word-0))
 
@@ -366,9 +375,11 @@ the beginning of the line"
     (projectile-with-default-dir (projectile-project-root)
       (compile full-cmd))))
 
-(defun cc-streamly ()
+(defun ccmain ()
   (interactive)
-  (cabalcc "streamly"))
+  (cabalcc (-last 's-present? (s-split "/" (projectile-project-root)))))
+
+(define-key haskell-mode-map (kbd "C-c C-c") 'ccmain)
 
 ;; Highlight emacs function calls
 (progn
@@ -414,3 +425,78 @@ the beginning of the line"
 	       (bounds (if (s-prefix? wor found-match) bounds-wor bounds-sym)))
 	  (progn (delete-region (car bounds) (cdr bounds))
 		 (insert found-match)))))))
+
+;; ===================================================================
+;; Custom function to delete blank lines & spaces, Thanks ergoemacs!
+(defun xah-delete-blank-lines ()
+  "Delete all newline around cursor.
+
+URL `http://ergoemacs.org/emacs/emacs_shrink_whitespace.html'
+Version 2018-04-02"
+  (interactive)
+  (let ($p3 $p4)
+          (skip-chars-backward "\n")
+          (setq $p3 (point))
+          (skip-chars-forward "\n")
+          (setq $p4 (point))
+          (delete-region $p3 $p4)))
+
+(defun xah-shrink-whitespaces ()
+  "Remove whitespaces around cursor to just one, or none.
+
+Shrink any neighboring space tab newline characters to 1 or none.
+If cursor neighbor has space/tab, toggle between 1 or 0 space.
+If cursor neighbor are newline, shrink them to just 1.
+If already has just 1 whitespace, delete it.
+
+URL `http://ergoemacs.org/emacs/emacs_shrink_whitespace.html'
+Version 2018-04-02T14:38:04-07:00"
+  (interactive)
+  (let* (
+         ($eol-count 0)
+         ($p0 (point))
+         $p1 ; whitespace begin
+         $p2 ; whitespace end
+         ($charBefore (char-before))
+         ($charAfter (char-after ))
+         ($space-neighbor-p (or (eq $charBefore 32) (eq $charBefore 9) (eq $charAfter 32) (eq $charAfter 9)))
+         $just-1-space-p
+         )
+    (skip-chars-backward " \n\t")
+    (setq $p1 (point))
+    (goto-char $p0)
+    (skip-chars-forward " \n\t")
+    (setq $p2 (point))
+    (goto-char $p1)
+    (while (search-forward "\n" $p2 t )
+      (setq $eol-count (1+ $eol-count)))
+    (setq $just-1-space-p (eq (- $p2 $p1) 1))
+    (goto-char $p0)
+    (cond
+     ((eq $eol-count 0)
+      (if $just-1-space-p
+          (delete-horizontal-space)
+        (progn (delete-horizontal-space)
+               (insert " "))))
+     ((eq $eol-count 1)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn (xah-delete-blank-lines) (insert " "))))
+     ((eq $eol-count 2)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn
+          (xah-delete-blank-lines)
+          (insert "\n"))))
+     ((> $eol-count 2)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn
+          (goto-char $p2)
+          (search-backward "\n" )
+          (delete-region $p1 (point))
+          (insert "\n"))))
+     (t (progn
+          (message "nothing done. logic error 40873. shouldn't reach here" ))))))
+
+(global-set-key (kbd "M-d") 'xah-shrink-whitespaces)
